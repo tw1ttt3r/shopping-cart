3873a6b14b5f18546011240fb66d0401c427504d
tw1ttt3r <tw1ttt3r@gmail.com>
2021-11-04 23:06:56 -0600

    feat: implement ProductList logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,47 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { ProductList } from '../components/ProductList'
+import { HomeState } from '../types/HomeState'
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ products, fetching }: any) => {
+
+  const [state, setState] = useState<HomeState>({
+    loading: true
+  })
+
+  useEffect(() => {
+    if (!fetching) {
+      setState({
+        loading: false,
+      })
+    }
+  }, [fetching])
+
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
-      <h1>Hello world!</h1>
+      { state.loading
+        ? <Image src="/loading.gif" width="100%" height="100%" />
+        : <div className="px-5 my-5 h-screen w-screen">
+          <ProductList products={products} />
+        </div>
+      }
     </>
   )
+}
+
+Home.getInitialProps = async (ctx) => {
+  let products = null
+  await fetch('https://products-api-meru.vercel.app/api/products', {
+    method: 'GET'
+  }).then( res => res.json())
+  .then( data => products = data)
+
+  return { products, fetching: false }
 }
 
 export default Home
