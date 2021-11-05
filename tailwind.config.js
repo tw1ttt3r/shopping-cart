9a5dc4d2c777b02c626da60313625d01f8da5182
tw1ttt3r <tw1ttt3r@gmail.com>
2021-11-04 23:05:15 -0600

    feat: spacing tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        120: '480px',
+        150: '600px'
+      }
+    },
   },
   variants: {
     extend: {},
