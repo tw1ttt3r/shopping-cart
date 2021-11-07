798853e0d6262c5e39f96c39c9244daaed7ccba5
tw1ttt3r <tw1ttt3r@gmail.com>
2021-11-06 21:43:25 -0600

    feat: implemented types

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,18 +1,28 @@
+import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import { ProductItemProps } from '../types/ProductItemProps';
 
-export function ProductItem({ product, key }: any) {
+export function ProductItem({ product, key }: ProductItemProps) {
     return(
-        <li key={key}>
-            <div className="flex flex-col h-120 w-150 p-4 shadow-sm border-2 cursor-pointer hover:bg-gray-100">
-                <h1>{product.name}</h1>
-                <Image src={product.cover} alt="product-image" width="100%" height="240" objectFit="contain" />
-                <p className="text-center">{new Intl.NumberFormat('es-MX', {style: 'currency', currency: 'MXN'}).format(product.price)}</p>
-                <p className="text-center">Cantidad: 0</p>
-                <div className="flex justify-center">
-                    <button className="w-8 h-8 text-lg border hover:bg-gray-200">+</button>
-                    <button className="w-8 h-8 text-lg border hover:bg-gray-200">-</button>
+        <React.Fragment key={key}>
+            <li>
+                <div className="flex flex-col h-120 w-150 p-4 shadow-sm border-2 cursor-pointer hover:bg-gray-100">
+                    <h1>{product.name}</h1>
+                    <Image src={product.cover} alt="product-image" width="100%" height="240" objectFit="contain" />
+                    <p className="text-center">
+                        {new Intl.NumberFormat('es-MX', {style: 'currency', currency: 'MXN'}).format(Number(product.price))}
+                    </p>
+                    <p className="text-center">Cantidad: 0</p>
+                    <div className="flex justify-center">
+                        <button className="w-8 h-8 text-lg border hover:bg-gray-200">+</button>
+                        <button className="w-8 h-8 text-lg border hover:bg-gray-200">-</button>
+                    </div>
+                    <Link href={`/${product.id}`}>
+                        <a className="text-indigo-500">Ver detalle</a>
+                    </Link>
                 </div>
-            </div>
-        </li>
+            </li>
+        </React.Fragment>
     )
 }
