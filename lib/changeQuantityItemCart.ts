26881d6244030a76e2117c898a220eb77e9daaaa
tw1ttt3r <tw1ttt3r@gmail.com>
2021-11-07 21:56:18 -0600

    feat: centralize function

---
+++ lib/changeQuantityItemCart.ts
@@ -0,0 +1,32 @@
+import { ContextInitialApp } from "@/types/ContextInitialApp";
+import { ItemCart } from "@/types/ItemCart";
+import { Product } from "@/types/Product";
+
+export default function changeQuantityItemCart(product: Product, action: string, context: ContextInitialApp, setContext: any) {
+
+    let cart: ItemCart[] = [];
+    if (context.cart.filter((p: ItemCart) => p.id === Number(product.id)).length === 0) {
+        cart = [...context.cart, { id: Number(product.id), quantity: 1 }]
+    } else {
+        cart = context.cart.reduce((acc: ItemCart[], curr: ItemCart) => {
+            if (curr.id === Number(product.id)) {
+                let quantity = curr.quantity;
+                if (action === 'add') {
+                    quantity += 1;
+                } else {
+                    if(quantity > 1) {
+                        quantity -= 1;
+                    } else {
+                        quantity = 0;
+                    }
+                }
+                return [...acc, { id: curr.id, quantity }];
+            }
+            return [...acc, curr];
+        }, [])
+    }
+    setContext({
+        ...context,
+        cart
+    })
+}
